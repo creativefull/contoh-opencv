8e1959c6675b0fa4ed6c54aa2cee624733d4f998
Shodiqul Muzaki <user.creativefull@gmail.com>
2016-03-27 01:02:45 +0700

    detect wajah

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 var cv = require('opencv');
-cv.readImage("./images/ktp.jpg", function(err, im){
-	var contour = im.findContours();
+var express = require('express'), app = express();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'jade');
+app.use(require('stylus').middleware(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './public')));
+
+
+var camera = new cv.VideoCapture(0);
+var line = new cv.Matrix(64,64);
+
+io.on('connection', function(socket) {
+	socket.on('playCamera', function(msg) {
+		setInterval(function() {
+			camera.read(function(err, im) {
+				im.detectObject(cv.FACE_CASCADE, {}, function(err, face) {
+					face.forEach(function(x) {
+						im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+					})
+					io.emit("image", im.toBuffer());
+				})
+			})
+		}, 300);
+	})
+})
+
+app.get('/', function(req,res,next) {
+	res.render('index');
+})
+
+http.listen(3000, function(req,res,next) {
+	console.log("Server Berjalan");
 })
